510dc50ae79a58fd0bc8f4d0378cdfd07b9d04e6
Tim Waterhouse <tim@timwaterhouse.com>
2024-11-27 15:02:44 -0800

    Initial setup and template

---
+++ template/src/main.rs
@@ -0,0 +1,28 @@
+use clap::Parser;
+use itertools::Itertools;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    data_file: String,
+    #[arg(long)]
+    debug: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let file = File::open(&args.data_file).expect("Failed to open file");
+    let reader = BufReader::new(file);
+    let lines: Vec<String> = reader
+        .lines()
+        .map(|line| line.expect("Failed to read line"))
+        .collect();
+
+    let chars = lines.iter().flat_map(|line| line.chars()).collect_vec();
+}
