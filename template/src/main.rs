625087f449f817ca1df2a5c24b5881806d8d4319
Tim Waterhouse <tim@timwaterhouse.com>
2024-12-01 21:24:54 -0800

    Improve template

--- template/src/main.rs
+++ template/src/main.rs
@@ -22,23 +22,23 @@ fn main() {
         args.data_file
     };
 
-    let (left, right) = parse(&data_file);
+    let data = parse(&data_file);
 
-    let result1 = part1(left.clone(), right.clone());
+    let result1 = part1(data.clone());
     println!("Part1: {}", result1);
 
-    println!("Part 2: {}", part2(left, right))
+    println!("Part 2: {}", part2(data))
 }
 
-fn part1(mut left: Vec<i64>, mut right: Vec<i64>) -> u64 {
+fn part1(data: Vec<Vec<i64>>) -> i64 {
     0
 }
 
-fn part2(left: Vec<i64>, right: Vec<i64>) -> i64 {
+fn part2(data: Vec<Vec<i64>>) -> i64 {
     0
 }
 
-fn parse(file: &str) -> (Vec<i64>, Vec<i64>) {
+fn parse(file: &str) -> (Vec<Vec<i64>>) {
     let file = File::open(file).expect("Failed to open file");
     let reader = BufReader::new(file);
     let lines: Vec<String> = reader
@@ -49,13 +49,11 @@ fn parse(file: &str) -> (Vec<i64>, Vec<i64>) {
     lines
         .iter()
         .map(|line| {
-            let (a, b) = line.split_ascii_whitespace().collect_tuple().unwrap();
-            (
-                a.parse::<i64>().expect("Failed to parse"),
-                b.parse::<i64>().expect("Failed to parse"),
-            )
+            line.split_ascii_whitespace()
+                .map(|v| v.parse::<i64>().unwrap())
+                .collect()
         })
-        .unzip()
+        .collect_vec()
 }
 
 #[cfg(test)]
@@ -64,15 +62,16 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let (left, right) = parse(&(env!("CARGO_MANIFEST_DIR").to_owned() + "/src/test1.txt"));
-        let result1 = part1(left, right);
+        let data = parse(&(env!("CARGO_MANIFEST_DIR").to_owned() + "/src/test1.txt"));
+        let result1 = part1(data);
+
         assert_eq!(result1, 0);
     }
 
     #[test]
     fn test_part2() {
-        let (left, right) = parse(&(env!("CARGO_MANIFEST_DIR").to_owned() + "/src/test1.txt"));
-        let result2 = part2(left, right);
+        let data = parse(&(env!("CARGO_MANIFEST_DIR").to_owned() + "/src/test1.txt"));
+        let result2 = part2(data);
 
         assert_eq!(result2, 0);
     }
