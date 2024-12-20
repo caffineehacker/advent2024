6359ec8a757d07608628d06fc19da19dda17edfc
Tim Waterhouse <tim@timwaterhouse.com>
2024-12-19 21:59:16 -0800

    Day 20, part 2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use itertools::Itertools;
 use std::{
     cmp::Reverse,
-    collections::{BinaryHeap, HashMap, HashSet, VecDeque},
+    collections::{BinaryHeap, HashMap, HashSet},
     fs::File,
     io::{BufRead, BufReader},
     ops::Add,
@@ -215,8 +215,173 @@ fn part1(input: &Input) -> usize {
         .count()
 }
 
-fn part2(input: &Input) -> i64 {
-    0
+fn part2(input: &Input) -> usize {
+    let max_bounds = Position {
+        x: input.walls.iter().map(|w| w.x).max().unwrap(),
+        y: input.walls.iter().map(|w| w.y).max().unwrap(),
+    };
+    // First find the best route, then we can start cheating
+    // Map from a Position to the number of seconds it takes to get to the end. This is without cheating.
+    let mut shortest_path_to_end: HashMap<Position, i64> = HashMap::new();
+
+    let mut states = BinaryHeap::new();
+    states.push(Reverse((0, input.exit)));
+
+    while states.len() > 0 {
+        let Reverse((time, pos)) = states.pop().unwrap();
+
+        if shortest_path_to_end.contains_key(&pos) {
+            continue;
+        }
+        shortest_path_to_end.insert(pos.clone(), time);
+
+        let new_pos = pos + Position { x: -1, y: 0 };
+        if pos.x > 0 && !input.walls.contains(&new_pos) {
+            states.push(Reverse((time + 1, new_pos)));
+        }
+
+        let new_pos = pos + Position { x: 1, y: 0 };
+        if pos.x < max_bounds.x && !input.walls.contains(&new_pos) {
+            states.push(Reverse((time + 1, new_pos)));
+        }
+
+        let new_pos = pos + Position { x: 0, y: -1 };
+        if pos.y > 0 && !input.walls.contains(&new_pos) {
+            states.push(Reverse((time + 1, new_pos)));
+        }
+
+        let new_pos = pos + Position { x: 0, y: 1 };
+        if pos.y < max_bounds.y && !input.walls.contains(&new_pos) {
+            states.push(Reverse((time + 1, new_pos)));
+        }
+    }
+
+    let shortest_no_cheat_path = shortest_path_to_end.get(&input.start).unwrap();
+    println!("Shortest path: {:?}", shortest_no_cheat_path);
+
+    let mut shortest_path_to_start: HashMap<Position, i64> = HashMap::new();
+
+    let mut states = BinaryHeap::new();
+    states.push(Reverse((0, input.start)));
+
+    while states.len() > 0 {
+        let Reverse((time, pos)) = states.pop().unwrap();
+
+        if shortest_path_to_start.contains_key(&pos) {
+            continue;
+        }
+        shortest_path_to_start.insert(pos.clone(), time);
+
+        let new_pos = pos + Position { x: -1, y: 0 };
+        if pos.x > 0 && !input.walls.contains(&new_pos) {
+            states.push(Reverse((time + 1, new_pos)));
+        }
+
+        let new_pos = pos + Position { x: 1, y: 0 };
+        if pos.x < max_bounds.x && !input.walls.contains(&new_pos) {
+            states.push(Reverse((time + 1, new_pos)));
+        }
+
+        let new_pos = pos + Position { x: 0, y: -1 };
+        if pos.y > 0 && !input.walls.contains(&new_pos) {
+            states.push(Reverse((time + 1, new_pos)));
+        }
+
+        let new_pos = pos + Position { x: 0, y: 1 };
+        if pos.y < max_bounds.y && !input.walls.contains(&new_pos) {
+            states.push(Reverse((time + 1, new_pos)));
+        }
+    }
+
+    let mut cheats = HashMap::new();
+    for y in 0..=max_bounds.y {
+        for x in 0..=max_bounds.x {
+            let pos = Position { x, y };
+
+            if input.walls.contains(&pos) {
+                continue;
+            }
+
+            // Can cheat for two steps, but really that's just into a wall and immediately out.
+            // The new time will be the steps from start + 2 (time in wall) + steps to end of the new place
+
+            let start_time = *shortest_path_to_start.get(&pos).unwrap();
+
+            for addx in 0..=20 {
+                for addy in 0..=20 {
+                    if addy + addx > 20 {
+                        continue;
+                    }
+
+                    let dest = pos + Position { x: addx, y: addy };
+                    if !(dest.x < 0
+                        || dest.y < 0
+                        || dest.x > max_bounds.x
+                        || dest.y > max_bounds.y
+                        || input.walls.contains(&dest))
+                    {
+                        cheats.insert(
+                            (pos, dest),
+                            start_time + addx + addy + shortest_path_to_end.get(&dest).unwrap(),
+                        );
+                    }
+
+                    let dest = pos + Position { x: -addx, y: addy };
+                    if !(dest.x < 0
+                        || dest.y < 0
+                        || dest.x > max_bounds.x
+                        || dest.y > max_bounds.y
+                        || input.walls.contains(&dest))
+                    {
+                        cheats.insert(
+                            (pos, dest),
+                            start_time + addx + addy + shortest_path_to_end.get(&dest).unwrap(),
+                        );
+                    }
+
+                    let dest = pos + Position { x: addx, y: -addy };
+                    if !(dest.x < 0
+                        || dest.y < 0
+                        || dest.x > max_bounds.x
+                        || dest.y > max_bounds.y
+                        || input.walls.contains(&dest))
+                    {
+                        cheats.insert(
+                            (pos, dest),
+                            start_time + addx + addy + shortest_path_to_end.get(&dest).unwrap(),
+                        );
+                    }
+
+                    let dest = pos + Position { x: -addx, y: -addy };
+                    if !(dest.x < 0
+                        || dest.y < 0
+                        || dest.x > max_bounds.x
+                        || dest.y > max_bounds.y
+                        || input.walls.contains(&dest))
+                    {
+                        cheats.insert(
+                            (pos, dest),
+                            start_time + addx + addy + shortest_path_to_end.get(&dest).unwrap(),
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    println!(
+        "Cheats: {:?}",
+        cheats
+            .iter()
+            .filter(|(_, t)| *t < shortest_no_cheat_path)
+            .sorted_by_key(|(_, t)| *t)
+            .count()
+    );
+
+    cheats
+        .iter()
+        .filter(|(_, t)| **t <= shortest_no_cheat_path - 100)
+        .count()
 }
 
 fn parse(file: &str) -> Input {
